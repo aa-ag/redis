c70a306a52d7bd1e1f19eba9230269b84e663122
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-08 18:09:40 -0500

    added base code for rq.py

--- rq.py
+++ rq.py
@@ -1,10 +1,12 @@
 ############------------ IMPORTS ------------############
-
-
-############------------ GLOBAL VARIABLE(S) ------------############
-
+from datetime import datetime, timedelta
+import time
 
 ############------------ FUNCTION(S) ------------############
+def print_task(seconds):
+    pass
 
+def print_numbers(seconds):
+    pass
 
 ############------------ DRIVER CODE ------------############
