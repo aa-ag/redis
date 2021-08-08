62ca2a1d351e1e02362f5661c12f2fbe2287f30b
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-08 18:11:06 -0500

    added logic for fucntion print_task

--- rq.py
+++ rq.py
@@ -4,9 +4,18 @@ import time
 
 ############------------ FUNCTION(S) ------------############
 def print_task(seconds):
-    pass
+    print("Starting task")
+
+    for second in range(seconds):
+        print(second, ". Hello World!")
+        time.sleep(1)
+
+    print("Task completed")
+
 
 def print_numbers(seconds):
     pass
 
 ############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    print_task()
