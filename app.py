fb44649a549aed755540e66313bebc88b9567f59
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-09 18:12:11 -0500

    current version printing time with 1 second intervals

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ queue = Queue(connection=Redis())
 
 ############------------ FUNCTION(S) ------------############
 def queue_tasks():
-    queue.enqueue(tasks.print_task, 5, retry=Retry(max=2))
-    queue.enqueue_in(timedelta(seconds=10), tasks.print_numbers, 5)
+    queue.enqueue(tasks.print_task, 1, retry=Retry(max=2))
+    # queue.enqueue_in(timedelta(seconds=10), tasks.print_numbers, 5)
 
 
 def main():
